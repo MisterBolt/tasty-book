cf859ad6c7b0e8323eee568c86f9745525003995
Grzetan <grzetan@gmail.com>
2021-07-13 11:31:37 +0200

    pick Configure User model and convert erb views to slim views

--- db/migrate/20210713082802_devise_create_users.rb
+++ db/migrate/20210713082802_devise_create_users.rb
@@ -4,11 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email, null: false, default: ""
+      t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      t.string :reset_password_token
+      t.string   :reset_password_token
       t.datetime :reset_password_sent_at
 
       ## Rememberable
