beabdf86c99efdfc93cf60506900a0209b3b79a7
Grzetan <grzetan@gmail.com>
2021-07-13 11:31:37 +0200

    Configure User model and convert erb views to slim views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :recipes
 
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
