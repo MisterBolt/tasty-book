80b94119851863d08aaf534f8f8bb4dc96ee4176
Paulina Markowska <markowskap1998@gmail.com>
2021-07-14 13:56:49 +0200

    Recipe model - changes
    1. I edited recipes controller
    2. I added routes
    3. I updated en.yml file

--- db/migrate/20210712203239_create_recipes.rb
+++ db/migrate/20210712203239_create_recipes.rb
@@ -1,9 +1,8 @@
 class CreateRecipes < ActiveRecord::Migration[6.1]
   def change
     create_table :recipes do |t|
-      t.string :title
+      t.string :title, null: false
       t.text :description
-
       t.timestamps
     end
   end
