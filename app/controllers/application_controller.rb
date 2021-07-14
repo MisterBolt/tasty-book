bcca513dea36c362127fe0643d7459f379489d8a
Grzetan <grzetan@gmail.com>
2021-07-14 20:35:12 +0200

    Added editing username and simplified registration controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_sign_up_params, only: [:create, :update]
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :terms, :location])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :terms, :location])
+    end
+
+    def configure_sign_up_params
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    end
 end
