b87e87dffbd86828063d64127c09cf3a00cd2e29
Grzetan <grzetan@gmail.com>
2021-07-15 20:14:51 +0200

    Updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,13 @@ ActiveRecord::Schema.define(version: 2021_07_13_082802) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "recipes", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -30,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_07_13_082802) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
