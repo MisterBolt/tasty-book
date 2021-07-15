c8b524e0e425000c6b4e19d612f27a5e1f5b78b8
Grzetan <grzetan@gmail.com>
2021-07-15 08:24:04 +0200

    Added username to email views and valitated username for presence

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,10 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_sign_up_params, only: [:create, :update]
-    before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :configure_user_params, only: [:create, :update], if: :devise_controller?
 
     protected
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :terms, :location])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :terms, :location])
-    end
-
-    def configure_sign_up_params
+    def configure_user_params
         devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:username])
     end
 end
