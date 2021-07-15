0083343d386654317004b4a90199f36bfac80702
bbronek <barbro214@gmail.com>
2021-07-16 00:06:59 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Seeding db: "
+
+user = User.where(username: "John Doe").first_or_create \
+  username:"John Doe", \
+  email: "john.doe@example.com", \
+  password: "password"
+
+print "."
+
+recipe = Recipe.where(title: "Apple Cake").first_or_create \
+  title: "Apple Cake",
+  description: "With chunks of sweet apples nestled in a tender and buttery rum cake, this apple cake is the essence of simplicity."
+
+print "."
+
+comment = Comment.where(body: "the best apple cake ever!").first_or_create \
+  body: "the best apple cake ever!", \
+  user: user, \
+  recipe: recipe 
+
+print "."
+
+puts "\nDB seeded!" 
