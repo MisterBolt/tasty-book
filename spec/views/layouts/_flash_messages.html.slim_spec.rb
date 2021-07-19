7a4e4ea815bb0d798822422361fbaf400661a3af
MisterBolt <bielecki.karol.private@gmail.com>
2021-07-19 19:46:27 +0200

    Add flash messages view tests

---
+++ spec/views/layouts/_flash_messages.html.slim_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe "layouts/_flash_messages", type: :view do
+  context "when flash message type is notice" do
+    before :each do
+      flash.notice = "Success"
+      render
+    end
+
+    it "displays green notice" do
+      expect(rendered).to match(/flash-success/)
+    end
+  end
+
+  context "when flash message type is alert" do
+    before :each do
+      flash.alert = "Error"
+      render
+    end
+
+    it "displays red alert" do
+      expect(rendered).to match(/flash-error/)
+    end
+  end
+end
