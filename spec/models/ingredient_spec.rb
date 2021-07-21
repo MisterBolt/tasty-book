ac078c0c340592591e5d56de8eb1a7b6e9cb65ca
Grzetan <grzetan@gmail.com>
2021-07-21 18:42:32 +0200

    Added model tests

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Ingredient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+    it { is_expected.to validate_length_of(:name).is_at_most(30) }
+  end
+
+  describe "associations" do
+    it { should have_many(:recipe_ingredients) }
+    it { should have_many(:recipes) }
+  end
 end
