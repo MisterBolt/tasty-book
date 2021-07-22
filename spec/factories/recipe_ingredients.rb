b993c91a1a644714bb468221ededc7041ff666a4
Grzetan <grzetan@gmail.com>
2021-07-22 07:53:19 +0200

    Deleted tests for join table

--- spec/factories/recipe_ingredients.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :recipe_ingredient do
-    
-  end
-end
--- spec/models/recipe_ingredient_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe RecipeIngredient, type: :model do
-  describe "associations" do
-    it { should belong_to(:recipe) }
-    it { should belong_to(:ingredient) }
-  end
-end
