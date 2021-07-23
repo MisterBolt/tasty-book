79439c096624c1d087f80b341d12afd82428fe68
gastronleroux <dejmieniv@gmail.com>
2021-07-23 09:45:01 +0200

    add methods to controller, add cook book to navbar

--- app/controllers/cook_books_controller.rb
+++ app/controllers/cook_books_controller.rb
@@ -1,7 +1,37 @@
 # frozen_string_literal: true
 
-class CookBooks < ApplicationController
+class CookBooksController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_cook_book, only: [:show]
+
   def index
     @cook_books = CookBook.all
+  end
+
+  def show
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @cook_book = CookBook.new(cook_book_params)
+    @cook_book.user = current_user
+    if @recipe.save
+      redirect_to(@recipe, notice: t(".notice"))
+    else
+      render(:new, status: :unprocessable_entity)
+    end
+  end
+
+  private
+
+  def set_cook_book
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def cook_book_params
+    params.require(:cook_book.permit(:title, recipe_ids: []))
   end
 end
--- spec/models/cook_book_spec.rb
+++ spec/models/cook_book_spec.rb
@@ -1,9 +1,7 @@
 require "rails_helper"
 
 RSpec.describe CookBook, type: :model do
-  subject { create(:cook_book, user: create(:user)) }
-  
-  it { should define_enum_for(:visibility) }
+  subject { create(:cook_book, user: create(:user), visibility: :public) }
 
   context "regarding validations" do
     it { is_expected.to validate_presence_of(:user_id) }
@@ -13,5 +11,11 @@ RSpec.describe CookBook, type: :model do
   context "regarding associations" do
     it { should belong_to(:user) }
     it { should have_and_belong_to_many(:recipes) }
+  end
+
+  context "regarding visibility" do
+    it { should define_enum_for(:visibility) }
+    it { expect(subject.visibility_public?).to eq true }
+    it { expect(subject.visibility_private?).to eq false }
   end
 end
