989546c0a52283268823ff2aee17e8dfeb86a100
gastronleroux <dejmieniv@gmail.com>
2021-07-24 18:19:10 +0200

    enable cook_book creation from user interface

--- app/controllers/cook_books_controller.rb
+++ app/controllers/cook_books_controller.rb
@@ -2,33 +2,30 @@
 
 class CookBooksController < ApplicationController
   before_action :authenticate_user!, only: [:create]
-  before_action :set_cook_book, only: [:show]
+  before_action :set_visibilities, only: [:index]
 
   def index
-    @recipe = Recipe.new
+    @cook_book = CookBook.new
     @cook_books = CookBook.all
-  end
-
-  def show
   end
 
   def create
     @cook_book = CookBook.new(cook_book_params)
     @cook_book.user = current_user
-    if @recipe.save
-      redirect_to(@recipe, notice: t(".notice"))
+    if @cook_book.save
+      redirect_to(cook_books_path, notice: t(".notice"))
     else
-      render(:new, status: :unprocessable_entity)
+      redirect_to(cook_books_path, alert: @cook_book.errors.full_messages[0])
     end
   end
 
   private
 
-  def set_cook_book
-    @recipe = Recipe.find(params[:id])
+  def set_visibilities
+    @visibilities = CookBook.visibilities.map { |key, value| [key.humanize, key] }
   end
 
   def cook_book_params
-    params.require(:cook_book.permit(:title, recipe_ids: []))
+    params.require(:cook_book).permit(:title, :visibility)
   end
 end
