8bb79fa3305af795a97c46695c5e7e415931baca
Grzetan <grzetan@gmail.com>
2021-07-29 08:25:15 +0200

    Deleted require_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,8 @@ class ApplicationController < ActionController::Base
   DEFAULT_PER_PAGE = 10
   before_action :configure_user_params, only: [:create, :update], if: :devise_controller?
 
-  def require_user
-      redirect_to new_user_session_path unless current_user
-  end
-
   def require_admin
-      require_user
+      authenticate_user!
       redirect_to new_user_session_path unless current_user.admin?
   end
 
