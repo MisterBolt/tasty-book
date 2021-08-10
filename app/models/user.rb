8ff46ad8836550844f4be9c06ae476b1b86d653b
bbronek <barbro214@gmail.com>
2021-08-10 23:43:48 +0200

    Add username validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
     :recoverable, :rememberable, :validatable
 
   validates :username, uniqueness: true, presence: true
+  validates_with UsernameValidator
 
   has_many :recipe_scores,
     dependent: :destroy
