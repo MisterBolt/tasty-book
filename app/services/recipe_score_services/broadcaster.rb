3c9a73795b69ec5297bd94eecceed4fa30f20b9f
Bakshee <bakshee.jw@gmail.com>
2021-08-23 19:18:10 +0200

    Add internationalization to mail notifications

--- app/services/recipe_score_services/broadcaster.rb
+++ app/services/recipe_score_services/broadcaster.rb
@@ -8,7 +8,7 @@ module RecipeScoreServices
       @recipe_score.broadcast_append_to :notifications,
         target: "#{@recipe_score.recipe.user.id}_toast",
         partial: "shared/toast",
-        locals: {message: I18n.t("notifications.recipe_score", recipe: @recipe_score.recipe.title, score: @recipe_score.score, user: @recipe_score.user.username)}
+        locals: {message: I18n.t("notifications.recipe_score", title: @recipe_score.recipe.title, score: @recipe_score.score, user: @recipe_score.user.username)}
     end
   end
 end
