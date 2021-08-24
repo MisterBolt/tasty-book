bce68e0fefe42029335fca567e8e19de89e00fa5
gastronleroux <dejmieniv@gmail.com>
2021-08-24 23:41:27 +0200

    rewrite navbar view tests

--- spec/views/layouts/_navbar.html.slim_spec.rb
+++ spec/views/layouts/_navbar.html.slim_spec.rb
@@ -1,28 +1,59 @@
 require "rails_helper"
 
 RSpec.describe "layouts/_navbar", type: :view do
-  before { render }
+  let(:user) { create(:user) }
 
-  context "when the user is logged in" do
-    let(:user) { create(:user) }
+  context "when user isn't logged in" do
+    before { visit root_path }
 
+    it { expect(page).to have_link(I18n.t("buttons.recipes"), class: "btn-nav", href: recipes_path) }
+    it { expect(page).to have_link(I18n.t("buttons.cook_books"), class: "btn-nav", href: cook_books_path) }
+    it { expect(page).to have_link(I18n.t("buttons.log_in"), class: "btn-nav", href: new_user_session_path) }
+    it { expect(page).to have_link(I18n.t("buttons.sign_up"), class: "btn-nav", href: new_user_registration_path) }
+    it { expect(page).not_to have_link(I18n.t("buttons.add_new_recipe")) }
+    it { expect(page).not_to have_css("#dropdown_navbar") }
+
+    it "highlight the recipes link after clicking on it" do
+      find("a", text: I18n.t("buttons.recipes")).click
+      expect(page).to have_link(I18n.t("buttons.recipes"), class: "btn-nav-active", href: recipes_path)
+    end
+
+    it "highlight the cook_books link after clicking on it" do
+      find("a", text: I18n.t("buttons.cook_books")).click
+      expect(page).to have_link(I18n.t("buttons.cook_books"), class: "btn-nav-active", href: cook_books_path)
+    end
+
+    it "highlight the login link after clicking on it" do
+      find("a", text: I18n.t("buttons.log_in")).click
+      expect(page).to have_link(I18n.t("buttons.log_in"), class: "btn-nav-active", href: new_user_session_path)
+    end
+
+    it "highlight the signup link after clicking on it" do
+      find("a", text: I18n.t("buttons.sign_up")).click
+      expect(page).to have_link(I18n.t("buttons.sign_up"), class: "btn-nav-active", href: new_user_registration_path)
+    end
+  end
+
+  context "when user is logged in" do
     before do
-      sign_in user
-      render
+      visit user_session_path
+      fill_in_and_log_in(user.email, user.password)
+      visit root_path
     end
 
-    it "displays \"Log out\" button" do
-      expect(rendered).to match(I18n.t("buttons.log_out"))
-    end
-  end
+    it { expect(page).to have_link(I18n.t("buttons.recipes"), class: "btn-nav-active", href: recipes_path) }
+    it { expect(page).to have_link(I18n.t("buttons.cook_books"), class: "btn-nav", href: cook_books_path) }
+    it { expect(page).not_to have_link(I18n.t("buttons.log_in"), class: "btn-nav", href: new_user_session_path) }
+    it { expect(page).not_to have_link(I18n.t("buttons.sign_up"), class: "btn-nav", href: new_user_registration_path) }
+    it { expect(page).to have_link(I18n.t("buttons.add_new_recipe"), class: "btn-nav", href: new_recipe_path) }
+    it { expect(page).to have_css("#dropdown_navbar") }
+    it { expect(page).to have_link(I18n.t("buttons.dashboard"), href: profile_index_path) }
+    it { expect(page).to have_link(I18n.t("buttons.my_profile"), href: user_path(user)) }
+    it { expect(page).to have_link(I18n.t("buttons.log_out"), href: destroy_user_session_path) }
 
-  context "when the user is not logged in" do
-    it "displays \"Log in\" button" do
-      expect(rendered).to match(I18n.t("buttons.log_in"))
+    it "highlight the add new recipe link after clicking on it" do
+      find("a", text: I18n.t("buttons.add_new_recipe")).click
+      expect(page).to have_link(I18n.t("buttons.add_new_recipe"), class: "btn-nav-active", href: new_recipe_path)
     end
-  end
-
-  it "displays \"Recipes\" button" do
-    expect(rendered).to match(I18n.t("buttons.recipes"))
   end
 end
