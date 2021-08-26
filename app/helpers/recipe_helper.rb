2803a75f09f0807f3a4e1add329b73d483f2438c
Bakshee <bakshee.jw@gmail.com>
2021-08-26 12:39:32 +0200

    Refactor recipe helper image methods

--- app/helpers/recipe_helper.rb
+++ app/helpers/recipe_helper.rb
@@ -14,26 +14,40 @@ module RecipeHelper
     }
   end
 
-  def recipe_image_for(recipe, img_variant)
-    image_tag(check_image(recipe, img_variant), class: "flex-none rounded-lg object-cover bg-gray-100", alt: "", size: "100x100")
+  IMAGE_CLASS = "flex-none rounded-lg object-cover bg-gray-100"
+  IMAGE_ALT = "image showing dish"
+  IMAGE_DEFAULT = "https://res.cloudinary.com/hp7f0176d/image/upload/v1629832221/sample/default_recipe_u9wld9.png"
+  IMAGE_THUMBNAIL_DEFAULT = "https://res.cloudinary.com/hp7f0176d/image/upload/v1629832231/sample/default_recipe_thumbnail_yarrcr.png"
+
+  def recipe_image_for_index(recipe)
+    if recipe.image.attached?
+      image_tag(recipe.image.variant(resize_to_fill: [100, 100]), class: IMAGE_CLASS, alt: IMAGE_ALT)
+    else
+      image_tag(IMAGE_THUMBNAIL_DEFAULT, class: IMAGE_CLASS)
+    end
   end
 
-  def check_image(recipe, img_variant)
+  def recipe_image_for_layout1(recipe)
     if recipe.image.attached?
-      case img_variant
-      when :thumbnail
-        recipe.image.variant(resize_to_fill: [100, 100])
-      when :layout1
-        recipe.image.variant(resize_to_fill: [1280, 1600])
-      when :layout2
-        recipe.image.variant(resize_to_fill: [1280, 256])
-      when :layout3
-        recipe.image
-      end
-    elsif img_variant == :thumbnail
-      "https://res.cloudinary.com/hp7f0176d/image/upload/v1629832231/sample/default_recipe_thumbnail_yarrcr.png"
+      image_tag(recipe.image.variant(resize_to_fill: [1280, 1600]), class: IMAGE_CLASS , alt: IMAGE_ALT)
     else
-      "https://res.cloudinary.com/hp7f0176d/image/upload/v1629832221/sample/default_recipe_u9wld9.png"
+      image_tag(IMAGE_DEFAULT, class: IMAGE_CLASS)
+    end
+  end
+
+  def recipe_image_for_layout2(recipe)
+    if recipe.image.attached?
+      image_tag(recipe.image.variant(resize_to_fill: [1280, 256]), class: IMAGE_CLASS, alt: IMAGE_ALT)
+    else
+      image_tag(IMAGE_DEFAULT, class: IMAGE_CLASS)
+    end
+  end
+
+  def recipe_image_for_layout3(recipe)
+    if recipe.image.attached?
+      image_tag(recipe.image, class: IMAGE_CLASS, alt: IMAGE_ALT)
+    else
+      image_tag(IMAGE_DEFAULT, class: IMAGE_CLASS)
     end
   end
 end
