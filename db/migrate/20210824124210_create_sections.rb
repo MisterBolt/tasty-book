5814eecd81b5f3e9d57f1be739b5f5d48a861753
Grzetan <grzetan@gmail.com>
2021-08-31 11:53:36 +0200

    Updated migration

--- db/migrate/20210824124210_create_sections.rb
+++ db/migrate/20210824124210_create_sections.rb
@@ -1,5 +1,8 @@
 class CreateSections < ActiveRecord::Migration[6.1]
-  def change
+  class Recipe < ApplicationRecord; end
+  class Section < ApplicationRecord; end
+
+  def up
     create_table :sections do |t|
       t.string :title, null: false
       t.text :body, null: false
@@ -8,6 +11,20 @@ class CreateSections < ActiveRecord::Migration[6.1]
       t.timestamps
     end
 
+    Recipe.find_each do |recipe|
+      Section.create!(recipe_id: recipe.id, title: "Description", body: recipe.preparation_description)
+    end
+
     remove_column :recipes, :preparation_description, :text
+  end
+
+  def down
+    add_column :recipes, :preparation_description, :text
+
+    Recipe.find_each do |recipe|
+      recipe.update!(preparation_description: Section.where(recipe_id: recipe.id).pluck(:description).join)
+    end
+
+    drop_table :sections
   end
 end
