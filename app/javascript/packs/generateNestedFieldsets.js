448bd6b40450210c0e2a4524d837a971dd3089e5
Grzetan <grzetan@gmail.com>
2021-08-31 11:05:33 +0200

    Deleted useless files and updated indexing

--- app/javascript/packs/generateNestedFieldsets.js
+++ app/javascript/packs/generateNestedFieldsets.js
@@ -2,7 +2,7 @@ document.querySelectorAll("a[data-form-prepend]").forEach(i => {
     i.addEventListener("click", e=>{
         e.preventDefault()
         let obj = document.createElement("fieldset")
-        obj.innerHTML = i.getAttribute("data-form-prepend").replaceAll("FIELDSET_INDEX", Math.floor(100000 + Math.random() * 900000));
+        obj.innerHTML = i.getAttribute("data-form-prepend").replaceAll("FIELDSET_INDEX", e.target.parentNode.childElementCount);
         obj.querySelector(".delete").addEventListener("click", e=>{
             e.preventDefault()
             e.target.parentNode.parentNode.remove()
